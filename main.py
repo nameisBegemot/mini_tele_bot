89910b93f2576b6ef3cbfd36df8a135a03fe7af2
nameisBegemot <134172076+nameisBegemot@users.noreply.github.com>
2023-09-29 20:47:14 +0300

    убрал лишнее
    проблема: телега шлет первое сообщение по кд

--- main.py
+++ main.py
@@ -3,85 +3,48 @@ import json
 import socket
 
 
-
-
-
-# def bot():
-#     TOKEN = '1808517150:AAH29icWhsWh-uBG3Icw10wqhYCsb8tf7IA'
-#     t = 'https://api.telegram.org/bot1808517150:AAH29icWhsWh-uBG3Icw10wqhYCsb8tf7IA/getUpdates'
-#     r = requests.get(t)
-#     print(r.json())
-
-
-gg = b'''HTTP/1.1 200 OK
-Server: YarServer/2009-09-09
-Content-Type: text/html
-Content-Length: 123
-Connection: close
-
-<h1> Hello </h1>'''
-
-
-f = b'''HTTP/1.1 200 OK
-Content-Type: text/html; charset=utf-8
-Content-Length: 16
-
-<h1> hello </h1>
-'''
-
 TOKEN = '1808517150:AAH29icWhsWh-uBG3Icw10wqhYCsb8tf7IA'
 methodUpdate = 'getUpdates'
 methodSendMsg = 'sendMessage'
 setWebhook = 'setWebhook'
 
-
 #https://api.telegram.org/bot1808517150:AAH29icWhsWh-uBG3Icw10wqhYCsb8tf7IA/setWebhook?url=https://d15d-31-181-124-249.ngrok-free.app
 #https://api.telegram.org/bot1808517150:AAH29icWhsWh-uBG3Icw10wqhYCsb8tf7IA/getWebhookInfo
-#https://api.telegram.org/bot1808517150:AAH29icWhsWh-uBG3Icw10wqhYCsb8tf7IA/setWebhook?url=https://9682-31-181-124-249.ngrok-free.app&drop_pending_updates=true
+#https://api.telegram.org/bot1808517150:AAH29icWhsWh-uBG3Icw10wqhYCsb8tf7IA/setWebhook?url=https://fd85-31-181-124-249.ngrok-free.app&drop_pending_updates=true
+
 
 def sendMSG(chat_id, text='hi'):
     url = f'https://api.telegram.org/bot{TOKEN}/{methodSendMsg}'
     a = {'chat_id': chat_id, 'text': text}
-    r = requests.post(url, json=a)
+    requests.post(url, json=a)
 
 
 def server():
     print("start")
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(('', 8443))
+    sock.bind(('', 9090))
     sock.listen()
 
-    conn, addr = sock.accept()
-
-    #print(conn, addr)
-
-
     while True:
-        data = conn.recv(1024)
+        conn, addr = sock.accept()
+        while True:
+            data = conn.recv(1024)
 
-        if not data:
-            break
-        print("-----------------")
+            if not data:
+                conn.close()
+                break
 
-        d = json.loads(data.decode().split("\r\n\r\n")[1])
+            else:
+                print("-----------------")
+                d = json.loads(data.decode().split("\r\n\r\n")[1])
+                chat = d['message']['chat']['id']
+                text = d['message']['text']
+                print(chat)
+                print(text)
 
-        chat = d['message']['chat']['id']
-        text = d['message']['text']
+                sendMSG(chat)
+        conn.close()
 
-        print(chat)
-        print(text)
-
-        sendMSG(chat)
-
-        #conn.send(f)
-
-    conn.close()
-
-
-def up():
-    api = f'https://api.telegram.org/bot{TOKEN}/{methodUpdate}'
-    r = requests.get(api)
-    print(r.json()['result'][-1]['message']['text'])
 
 server()
